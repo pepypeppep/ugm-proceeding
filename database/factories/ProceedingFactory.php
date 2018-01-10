497eb72c420ebb77329836227a56dc64abc39f1d
w1lldone <wildan.arrahman@gmail.com>
2018-01-11 01:16:51 +0700

    fix table and factory

--- database/factories/ProceedingFactory.php
+++ database/factories/ProceedingFactory.php
@@ -6,6 +6,8 @@ $factory->define(App\Proceeding::class, function (Faker $faker) {
     return [
         'name' => $faker->words(4, true),
         'alias' => strtoupper($faker->bothify('IC?? 201#')),
+        'conference_start' => '2017-10-12',
+        'conference_end' => '2017-10-13',
         'front_cover' => $faker->imageUrl(344, 550, 'technics'),
         'back_cover' => $faker->imageUrl(344, 550, 'technics'),
         'isbn' => $faker->isbn13,
