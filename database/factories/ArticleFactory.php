5256d3208cc86d2d44e6b95acf9377bfc954e10d
w1lldone <wildan.arrahman@gmail.com>
2018-01-11 00:30:21 +0700

    fix factories

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -4,8 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Article::class, function (Faker $faker) {
     return [
+    	'proceeding_id' => 1,
         'title' => $faker->sentence($faker->numberBetween(6, 10), true),
-        'abstract' => $faker->paragraph(6, true),
+        'abstract' => $faker->paragraph(10, true),
         'keywords' => $faker->word.','.$faker->word.','.$faker->word,
         'end_page' => 26,
         'start_page' => 13,
--- database/factories/ProceedingFactory.php
+++ database/factories/ProceedingFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Proceeding::class, function (Faker $faker) {
     return [
-        'name' => $faker->sentence(4, true),
-        'alias' => $faker->bothify('IC## 201?'),
+        'name' => $faker->words(4, true),
+        'alias' => strtoupper($faker->bothify('IC?? 201#')),
         'front_cover' => $faker->imageUrl(344, 550, 'technics'),
         'back_cover' => $faker->imageUrl(344, 550, 'technics'),
         'isbn' => $faker->isbn13,
