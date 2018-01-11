65d717306c94bc5e4f06faefde7576fb9dcadfdb
w1lldone <wildan.arrahman@gmail.com>
2018-01-11 10:47:49 +0700

    add and implement articles API resources

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Articles;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+    	return Articles::collection(Article::with('author')->paginate(5));
+    }
+}
---
+++ app/Http/Resources/Articles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class Articles extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'abstract' => $this->abstract,
+            'keywords' => $this->keyword,
+            'start_page' => $this->start_page,
+            'end_page' => $this->end_page,
+            'views' => $this->view,
+            'downloads' => $this->downloads,
+            'cites' => $this->cites,
+            'identifier' => $this->article_identifier()->get(['type', 'code']),
+            'authors' => $this->whenLoaded('author'),
+            'date_added' => $this->created_at->format('j F Y'),
+        ];
+    }
+}
