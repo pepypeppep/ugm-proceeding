c19b50f8665654b716ede4926d3e41421f7922a2
w1lldone <wildan.arrahman@gmail.com>
2018-01-11 22:22:29 +0700

    fix import

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
-use App\Repositories\Users;
+use App\Repositories\Api\Users;
 use App\Http\Resources\UsersCollection;
 use App\Http\Resources\Users as UsersResource;
 
