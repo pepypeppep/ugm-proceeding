58c68f67890f6fe65d5b326bc6f82b7fbba95011
w1lldone <wildan.arrahman@gmail.com>
2018-01-23 13:35:04 +0700

    add guarded property to models

--- app/Institution.php
+++ app/Institution.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Institution extends Model
 {
+	protected $guarded = ['id'];
+	
     public function user(){
     	return $this->hasMany('App\User');
     }
