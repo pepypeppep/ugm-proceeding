da7c5345ad9ae824d7625a09495d9e721a8abc33
pepypeppep <pepy.peppep@gmail.com>
2018-02-07 09:23:51 +0700

    Fix icon

--- resources/views/dashboard/proceeding/sidebar.blade.php
+++ resources/views/dashboard/proceeding/sidebar.blade.php
@@ -2,16 +2,16 @@
   <ul class="nav nav-pills flex-column sticky-top sticky-nav pb-3" style="border-bottom: 1px solid rgba(0, 0, 0, 0.125);">
   <h5>Status</h5>
     <li class="nav-item">
-      <a class="nav-link @if(request('tab') == 'recent' || request('tab') == null) {{ 'active' }} @endif" href="?tab=recent"><i class="fa fa-file-text-o fa-fw"></i>Recent</a>
+      <a class="nav-link @if(request('tab') == 'recent' || request('tab') == null) {{ 'active' }} @endif" href="?tab=recent"><i class="far fa-file-alt fa-fw mr-2"></i>Recent</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link @if(request('tab') == 'draft') {{ 'active' }} @endif" href="?tab=draft"><i class="fa fa-pencil-square-o fa-fw"></i>Draft</a>
+      <a class="nav-link @if(request('tab') == 'draft') {{ 'active' }} @endif" href="?tab=draft"><i class="fas fa-edit fa-fw mr-2"></i>Draft</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link @if(request('tab') == 'published') {{ 'active' }} @endif" href="?tab=published"><i class="fa fa-paper-plane-o fa-fw"></i>Published</a>
+      <a class="nav-link @if(request('tab') == 'published') {{ 'active' }} @endif" href="?tab=published"><i class="far fa-paper-plane fa-fw mr-2"></i>Published</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link @if(request('tab') == 'trashed') {{ 'active' }} @endif" href="?tab=trashed"><i class="fa fa-trash-o fa-fw"></i>Deleted</a>
+      <a class="nav-link @if(request('tab') == 'trashed') {{ 'active' }} @endif" href="?tab=trashed"><i class="far fa-trash-alt fa-fw mr-2"></i>Deleted</a>
     </li>
   </ul>
 </div>
