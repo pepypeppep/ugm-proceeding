c21ed3d29661bc2fa3eae27a09dc7bb1ed9e3c54
w1lldone <wildan.arrahman@gmail.com>
2018-03-01 08:51:30 +0700

    fix href on proceedings public page

--- resources/views/public/home/proceedings.blade.php
+++ resources/views/public/home/proceedings.blade.php
@@ -68,7 +68,7 @@
 				<div class="card-deck">
 					@foreach($proceedings->data->take(20) as $proceeding)
 					<div class="card book">
-						<a href="./docs/201709.html"><img class="card-img-top" src="{{ $proceeding['front_cover'] }}" alt="{{ $proceeding['alias'] }}" title="{{ $proceeding['name'] }}"></a>
+						<a href="{{ route('public.proceeding.show', $proceeding['id']) }}"><img class="card-img-top" src="{{ $proceeding['front_cover'] }}" alt="{{ $proceeding['alias'] }}" title="{{ $proceeding['name'] }}"></a>
 					</div>
 					@endforeach
 				</div>
